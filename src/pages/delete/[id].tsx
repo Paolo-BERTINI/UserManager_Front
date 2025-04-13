0b936c1ca362126a6ff3b32550a766f734cac8d7
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-04-13 23:05:01 +0200

    delete

---
+++ src/pages/delete/[id].tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useRouter } from 'next/router';
+
+export default function DeleteUser() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const handleDelete = async () => {
+    await fetch(`http://localhost:3001/users/${id}`, {
+      method: 'DELETE',
+    });
+  };
+
+  return (
+    <div>
+      <p>Supprimer l’utilisateur ?</p>
+      <button onClick={handleDelete}>Oui</button>
+    </div>
+  );
+}
