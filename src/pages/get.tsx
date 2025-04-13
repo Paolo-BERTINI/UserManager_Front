a0bce749027fc58889fbc6e56f11ba4685b0198d
Paolo BERTINI <bertin_p@etna-alternance.net>
2025-04-13 22:28:33 +0200

    get qui etait dans la page d'acceuil est mtn dans sa prope page

---
+++ src/pages/get.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+export default function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error('Erreur de chargement des utilisateurs:', err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Liste des utilisateurs</h1>
+      <ul>
+        {users.map((user: any) => (
+          <li key={user.id}>
+            <p>ID : {user.id}</p>
+            <p>Prénom : {user.first_name}</p>
+            <p>Nom : {user.first_name}</p>
+            <p>Email : {user.mail}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
